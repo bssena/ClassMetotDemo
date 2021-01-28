40a6c044c893c1cb1d3b7237038fc156ada9a1a9
sena bilgin <sena.bilgin@ogr.sakarya.edu.tr>
2021-01-28 20:39:25 +0300

    .

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + " " + musteri.Ad + " " + " " + musteri.Soyad+ " " + "eklendi");
+
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + " " + musteri.Ad + " " + " " + musteri.Soyad +" " +  "silindi");
+        }
+
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + " " + musteri.Ad + " " + " " + musteri.Soyad + " " + "güncellendi");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "  " + " " + musteri.Ad + " " + " " + musteri.Soyad  );
+        }
+
+
+    }
+
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri { Id = 1, Ad = "seda", Soyad = "akın" };
+
+            Musteri musteri2 = new Musteri { Id = 2, Ad = "mirem", Soyad="ışıldak" };
+
+            Musteri musteri3 = new Musteri { Id = 3, Ad = "melda", Soyad = "köse" };
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Add(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.Update(musteri3);
+
+            Console.WriteLine("------LİSTELEME------");
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+
+
+
+
+        }
+    }
+}
